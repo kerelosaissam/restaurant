a97de8b24dd9fcbfbba6341accc55fda0cac376e
kerelos aissam <kerelos2soliman004@gmail.com>
2026-01-12 19:59:48 +0200

    resturant

---
+++ lib/main.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant/widgets/addremovefun.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int num = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.orange[100],
+        appBar: AppBar(
+          backgroundColor: Colors.orange[300],
+          leading: const Icon(Icons.arrow_back_ios),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    'assets/Ramadan.jpg',
+                    width: double.infinity,
+                    height: 350,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Text(
+                  "Burger Mix Combo",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFB36D5D),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.star, color: Color(0xFFB36D5D)),
+                        SizedBox(width: 5),
+                        Text(
+                          "4.5",
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+
+                    Container(
+                      width: 160,
+                      height: 60,
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 231, 143, 83),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Row(
+                        children: [
+                          Addremovefun(
+                            color: const Color.fromARGB(255, 248, 207, 168),
+                            ontap: () {
+                              setState(() {
+                                if(num>0){
+                                  num--;
+                                };
+                              });
+                            },
+                            icon: const Icon(Icons.remove),
+                            iconcolor: Color(0xFFB36D5D),
+                          ),
+
+                          Expanded(
+                            child: Center(
+                              child: Text(
+                                num.toString(),
+                                style: const TextStyle(
+                                  fontSize: 22,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Addremovefun(
+                            color: const Color(0xFFB36D5D),
+                            ontap: () {
+                              setState(() {
+                                num++;
+                              });
+                            },
+                            icon: const Icon(Icons.add),
+                            iconcolor: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Description",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFB36D5D),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  "2 Burger + fries + drink with 30% sale",
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "EGP  160",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+
+                        fontSize: 15,
+                      ),
+                    ),
+                    Container(
+                      width: 120,
+                      height: 45,
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        color:  Color.fromARGB(255, 231, 143, 83),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Center(child: Text("Add To Cart",
+                      style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),
+                      )),
+                      )
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Divider(color:Color.fromARGB(255, 231, 143, 83) ,
+                thickness: 2,
+                ),
+                SizedBox(height: 10,),
+                Container(
+                  width: double.infinity,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.orange[300]
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Review",
+                            style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromARGB(255, 153, 88, 73),
+                    ),
+                            ),
+                            SizedBox(height: 5,),
+                            Text("send your feedback",
+                            style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                            ),
+                          ],
+                        ),
+                        Icon(Icons.arrow_drop_down,color:Color.fromARGB(255, 153, 88, 73) ,)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/addremovefun.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Addremovefun extends StatelessWidget {
+  final Color color;
+  final VoidCallback ontap;
+  final Icon icon;
+  final Color iconcolor;
+  const Addremovefun({super.key, required this.color, required this.ontap, required this.icon, required this.iconcolor});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child:  Icon(icon.icon, color: iconcolor),
+      ),
+    );
+  }
+}
